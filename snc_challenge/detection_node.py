eb28e1347032b02b664e85528e0f208704e69336
Xiaona <s3825356@student.rmit.edu.au>
2025-04-10 14:29:31 +1000

    fix: change detection to basic code structure

--- snc_challenge/detection_node.py
+++ snc_challenge/detection_node.py
@@ -2,53 +2,13 @@
 
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image, LaserScan
-from visualization_msgs.msg import Marker
-from cv_bridge import CvBridge
-from tf2_ros import TransformException, Buffer, TransformListener
 
 class DetectionNode(Node):
 
     def __init__(self):
         super().__init__('detection_node')
 
-        # Subscribe to Camera
-        self.img_sub = self.subscriptions(
-            Image, '/camera/image_raw', self.image_callback, 10)
-        
-        # Subscribe to Laser
-        self.laser_sub = self.subscriptions(
-            LaserScan, '/scan', self.laser_callback, 10)
-
-        # publish the hazard symbols
-        self.marker_pub = self.create_publisher(
-            Marker, '/hazards', 10)
-
-        # TF
-        self.tf_buffer = Buffer()
-        self.tf_listener = TransformListener(self.tf_buffer, self)
-        
-        # Storing detected markers
-        self.marker_ids = {}
-        self.bridge = CvBridge()
         self.get_logger().info("Detection Node Ready")
-        
-
-    def image_callback(self, msg):
-        # Image processing logic 
-        # TO DO...
-
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-            # Call find_object_2d to detect markers...
-            # TO DO...
-        except Exception as e:
-            self.get_logger().error(f"Image processing error: {str(e)}")
-
-    def laser_callback(self, msg):
-        # Laser Data Processing
-        # TO DO...
-        pass
             
 def main(args = None):
     rclpy.init(args = args)
