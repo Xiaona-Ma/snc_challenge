0517a90c4a63e3264976b4db5a9a978427969d8c
Ellen Dinata <s4072221@student.rmit.edu.au>
2025-04-15 20:07:38 +1000

    add slam to launch

--- launch/challenge.launch.py
+++ launch/challenge.launch.py
@@ -1,7 +1,16 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    # Path to slam.yaml
+    slam_config_path = os.path.join(
+        get_package_share_directory('snc_challenge'),
+        'config',
+        'slam.yaml'
+    )
+
     return LaunchDescription([
         Node(
             package='snc_challenge',
@@ -17,5 +26,12 @@ def generate_launch_description():
             package='snc_challenge',
             executable='tracking_node_executable',
             output='screen'
+        ),
+        Node(
+            package='slam_toolbox',
+            executable='async_slam_toolbox_node',
+            name='slam_toolbox',
+            output='screen',
+            parameters=[slam_config_path]
         )
     ])
